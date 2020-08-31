43c044b80c8f4defc52f5aca3ec3e1ebd6a17b9f
demon28 <274733956@qq.com>
2020-08-31 17:18:22 +0800

    随意提交

--- YH.ASM.App/static/js/Config.js
+++ YH.ASM.App/static/js/Config.js
@@ -3,8 +3,8 @@
 const Parameters={
 	
 	 
-	//是否是开发环境： 1： 开发环境， 2：预投产环境，3，正式环境
-	Isdevpos:2,   
+	//是否是开发环境： 1： 开发环境， 2：预投产环境，3，正式环境，4，和合售后，5，雅康售后
+	Isdevpos:5,   
 	Version:"1.1.31",
 	ApiKey:"58b59b3ae5d0ec0629950ebdd4dabe39",
 	LoginHost:function(){
@@ -16,6 +16,12 @@ const Parameters={
 				}
 				if(Parameters.Isdevpos==3){
 					return "https://asm.yhwins.com:52420";
+				}
+				if(Parameters.Isdevpos==4){
+					return "https://hhasm.yhwins.com";
+				}
+				if(Parameters.Isdevpos==5){
+					return "https://ykasm.yhwins.com";
 				}
 				
 		},
@@ -29,6 +35,12 @@ const Parameters={
 		}
 		if(Parameters.Isdevpos==3){
 			return "正式版";
+		}
+		if(Parameters.Isdevpos==4){
+			return "和合版";
+		}
+		if(Parameters.Isdevpos==5){
+			return "雅康版";
 		}
 		
 	}
